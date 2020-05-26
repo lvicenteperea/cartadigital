244b5f0026c7f27419a8e36d7a7fae71e2a10f18
“LAVP” <lavp99@hotmail.com>
2020-05-26 03:33:02 +0200

    pequeños cambios en menú superior

--- resources/views/includes/menu_superior.blade.php
+++ resources/views/includes/menu_superior.blade.php
@@ -1,24 +1,59 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="{{ route('dashboard') }}">{{ Config::get('app.name') }}</a>
-    </div>
-    <div id="navbar" class="navbar-collapse collapse">
-        <ul class="nav navbar-nav navbar-right">
-        <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
-        <li><a href="{{ route('dashboard') }}">Settings</a></li>
-        <li><a href="{{ route('dashboard') }}">Profile</a></li>
-        <li><a href="{{ route('dashboard') }}">Help</a></li>
-        </ul>
-        <form class="navbar-form navbar-right">
-        <input type="text" class="form-control" placeholder="Search...">
-        </form>
-    </div>
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ route('dashboard') }}">{{ Config::get('app.name') }}</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                <li><a href="{{ route('dashboard') }}">Settings</a></li>
+                @guest
+                    <!-- li class="nav-item"> <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li   -->
+                    <li><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                    @if (Route::has('register'))
+                        <!-- li class="nav-item"> <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li -->
+                        <li> <a href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                    @endif
+                @else
+                    <!-- li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->nombre }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li   -->
+
+                    <li>
+                        <a href="#" role="button"aria-haspopup="true" aria-expanded="false" v-pre>{{ Auth::user()->nombre }} <span class="caret"></span></a>
+                        <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                @endguest
+            </ul>
+            <form class="navbar-form navbar-right">
+                <input type="text" class="form-control" placeholder="Search...">
+            </form>
+        </div>
     </div>
 </nav>
